2f90cfc7f534543232ecbf3d914766c189505d5d
Dirceu Schlickmann <dschli2007@gmail.com>
2023-02-28 21:33:23 -0300

    Ajustes

--- node/index.js
+++ node/index.js
@@ -11,13 +11,21 @@ const config = {
 const mysql = require('mysql')
 const connection = mysql.createConnection(config)
 const sql = `select name from people order by name`
-
+const createTable = `create table people (id int not null auto_increment, name varchar(100) not null, primary key (id), unique(name))`
+const insertPeople = `insert into people (name) values ('John Wick')`
 let pessoas = "nenhum";
 
-connection.query(sql, (err, result, fields) => {
-    if (err) throw err;
-    pessoas = result.reduce((acc, obj) => { return acc + '<p>' + obj.name + '</p>'}, "")
-})
+connection.query(createTable, (err, result, fields) => { });
+connection.query(insertPeople, (err, result, fields) => { });
+
+connection.query(sql,
+    (err, result, fields) => {
+        if (err)
+            throw err;
+
+        pessoas = result.reduce((acc, obj) => { return acc + '<p>' + obj.name + '</p>' }, "");
+    });
+
 connection.end()
 
 app.get('/', (req, res) => {
